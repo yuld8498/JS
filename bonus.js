244e2078ff52cea30e1bdf4c691a7c39e020bdd9
yuld8498 <truongthanh11201@gmail.com>
2022-05-15 21:47:19 +0700

    15/05

--- bonus.js
+++ bonus.js
@@ -1,6 +1,10 @@
 // let age = 0;
 // console.log(age<1?"nhập lại tuổi":age<3?"trẻ sơ sinh":age<11?"nhi đồng":age<18?"vị thành niên":age<40?"thanhnien":age<51?"trung niên":"cao niên")
-let thisyear = 2022;
-let yofm = 2008;
-let age = thisyear - yofm;
-console.log(age>14?"thay thế":age>9?"bảo trì":"không có đề xuất")
+//3
+// let thisyear = 2022;
+// let yofm = 2008;
+// let age = thisyear - yofm;
+// console.log(age>14?"thay thế":age>9?"bảo trì":"không có đề xuất")
+//4
+let diemtb = 7.9;
+console.log(diemtb>10?"nhập lại điểm":diemtb)
