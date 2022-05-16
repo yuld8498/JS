50427a8657c586579008b3cdb6694448975f9eee
yuld8498 <truongthanh11201@gmail.com>
2022-05-16 16:48:54 +0700

    Add

---
+++ loopp.js
@@ -0,0 +1,32 @@
+// let total=0;
+// let count=0;
+// for (i = 0 ;count <10;i++){
+//     if (i%5==0) {
+//         total +=i;
+//         count++;
+//     } 
+// }
+// console.log(sum);
+// let count =0;
+// let total =0;
+// let i=0;
+// while(count <10){
+//     if(i%5==0){
+//         count++;
+//         total +=i;
+//     }
+//     i++;
+// }
+// console.log(total)
+// let count =0;
+// let total =0;
+// let i=0;
+// do {
+//     if( i%5==0){
+//         total +=i;
+//         count++;
+//     }
+//     i++;
+// } 
+// while (count <10);
+// console.log(total)
